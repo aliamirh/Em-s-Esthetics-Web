f26c5b0543164deec929a12acc24025c7c2b687a
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-06 15:42:28 -0800

    add css style sheet to change background on all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../logo.jpg';
+import { Spring } from 'react-spring/renderprops';
 
 const Header = () => {
   return (
-    <div>
-      <img style={headerLogo} src={Logo} alt='logo' />
-      <div style={headerLinksDiv}>
-        <ul style={linkUnordered}>
-          <li style={linkListItem}>
-            <Link style={linkTo} to='/'>
-              Home
-            </Link>
-          </li>
-          <li style={linkListItem}>
-            <Link style={linkTo} to='/services'>
-              Services
-            </Link>
-          </li>
-          <li style={linkListItem}>
-            <Link style={linkTo} to='/contact'>
-              Contact
-            </Link>
-          </li>
-          <li style={linkListItem}>
-            <Link style={linkTo} to='./portfolio'>
-              Portfolio
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {props => (
+        <div style={props}>
+          <img style={headerLogo} src={Logo} alt='logo' />
+          <div style={headerLinksDiv}>
+            <ul style={linkUnordered}>
+              <li style={linkListItem}>
+                <Link style={linkTo} to='/'>
+                  Home
+                </Link>
+              </li>
+              <li style={linkListItem}>
+                <Link style={linkTo} to='/services'>
+                  Services
+                </Link>
+              </li>
+              <li style={linkListItem}>
+                <Link style={linkTo} to='/contact'>
+                  Contact
+                </Link>
+              </li>
+              <li style={linkListItem}>
+                <Link style={linkTo} to='./portfolio'>
+                  Portfolio
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </Spring>
   );
 };
 
