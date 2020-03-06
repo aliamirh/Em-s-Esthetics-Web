db238ff597bd076082d1216b57e33cc8ebfd4463
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-06 12:21:44 -0800

    add company logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <h1>Em's Esthetics</h1>
-      <div>
+      <h1 style={headerLogo}>Em's Esthetics</h1>
+      <div style={headerLinks}>
         <Link to='/'>Home</Link> | <Link to='/services'>Services</Link> |
         <Link to='/contact'>Contact</Link> |
         <Link to='./portfolio'>Portfolio</Link>
@@ -14,4 +14,12 @@ const Header = () => {
   );
 };
 
+const headerLinks = {
+  textAlign: 'center',
+  backgroundColor: '#a7cdd1'
+};
+
+const headerLogo = {
+  textAlign: 'center'
+};
 export default Header;
