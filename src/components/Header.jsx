e09cf1aa0695c08c5e6fb57ef10f6d3c7fe9d5f0
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-06 12:00:28 -0800

    add links to different sections of the web page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
       <h1>Em's Esthetics</h1>
+      <div>
+        <Link to='/'>Home</Link> | <Link to='/services'>Services</Link> |
+        <Link to='/contact'>Contact</Link> |
+        <Link to='./portfolio'>Portfolio</Link>
+      </div>
     </div>
   );
 };
