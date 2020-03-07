bca1d96180d53d23c9a946ed33769c9500ea53c5
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-06 16:29:18 -0800

    add picture of the owner, still styling and deciding positioning of images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Ems from '../fillerImages/emily.jpg';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img style={emsPhoto} src={Ems} alt='Owner' />
+        <div style={textOverflow}>
+          <h2>T.Emily Amirhamzeh</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const emsPhoto = {
+  width: '20%',
+  borderRadius: '20%'
+};
+
+const textOverflow = {
+  position: 'absolute',
+  top: '68%',
+  left: '10%',
+  transform: 'translate(-50%, -50%)',
+  fontSize: '27px',
+  paddingLeft: '2px',
+  color: '#72a675'
 };
 
 export default Home;
