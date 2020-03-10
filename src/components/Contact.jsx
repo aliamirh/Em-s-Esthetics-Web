4d01c980b4a7d9cbd0e636cae66c786d9fd37572
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-09 20:27:55 -0700

    add google maps api

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const Contact = () => {
-  return (
-    <div>
+class Contact extends React.Component {
+  render() {
+    return (
       <div>
-        <h1>Contact</h1>
+        <Map
+          google={this.props.google}
+          zoom={20}
+          style={mapStyles}
+          initialCenter={{ lat: 45.6775868, lng: -122.6706828 }}
+        >
+          <Marker
+            position={{ lat: 45.6775868, lng: -122.6706828 }}
+            text='Ems Esthetics'
+          />
+        </Map>
+        <div>
+          <h1>Contact</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
 };
 
-export default Contact;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_API_KEY
+})(Contact);
