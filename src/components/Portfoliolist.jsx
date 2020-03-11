62907cb4cb6d23a302e856e46308e956e38f5030
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-11 16:24:20 -0700

    debugging map

---
+++ src/components/Portfoliolist.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import client from '../clients/clients.jpg';
+import client1 from '../clients/clients.jpg';
+import client2 from '../clients/clients.jpg';
+import client3 from '../clients/clients.jpg';
+import client4 from '../clients/clients.jpg';
+import client5 from '../clients/clients.jpg';
+
+const clientPhotos = [
+  <img src={client} alt='client' />,
+  <img src={client1} alt='client1' />,
+  <img src={client2} alt='client2' />,
+  <img src={client3} alt='client3' />,
+  <img src={client4} alt='client4' />,
+  <img src={client5} alt='client5' />
+];
+
+function Portfoliolist() {
+  return <div></div>;
+}
+
+export default Portfoliolist;
