3710abf20d74c49776e5aad269edbf86323a6243
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-11 14:44:21 -0700

    animated practice

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const Portfolio = () => {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+
   return (
     <div>
       <h1>Portfolio</h1>
