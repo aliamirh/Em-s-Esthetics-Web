0def0368856e7f637799e6c33b525d15a97d79fa
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-11 18:27:21 -0700

    debug issues with transition animation

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import client from '../clients/client.jpg';
-import client1 from '../clients/client2.jpg';
-import client2 from '../clients/client3.jpg';
-import client3 from '../clients/client4.jpg';
-import client4 from '../clients/client5.jpg';
+import client2 from '../clients/client2.jpg';
+import client3 from '../clients/client3.jpg';
+import client4 from '../clients/client4.jpg';
+import client5 from '../clients/client5.jpg';
+import client6 from '../clients/client6.jpg';
+import client7 from '../clients/client7.jpg';
+import client8 from '../clients/client8.jpg';
+import client9 from '../clients/client9.jpeg';
+import client10 from '../clients/client10.jpeg';
+import client11 from '../clients/client11.jpeg';
+import client12 from '../clients/client12.jpg';
+import client13 from '../clients/client13.jpg';
 import wedding from '../wedding/wedding.jpg';
 import wedding2 from '../wedding/wedding2.jpg';
 
@@ -19,17 +27,25 @@ const Portfolio = () => {
   return (
     <div>
       <animated.div style={fade}>
-        <h1>Portfolio</h1>
+        <h1 style={headerFont}>Portfolio</h1>
         <div>
           <img style={scaleClientImages} src={client} alt='client' />
-          <img style={scaleClientImages} src={client1} alt='client2' />
-          <img style={scaleClientImages} src={client2} alt='client3' />
-          <img style={scaleClientImages} src={client3} alt='client4' />
-          <img style={scaleClientImages} src={client4} alt='client5' />
+          <img style={scaleClientImages} src={client2} alt='client2' />
+          <img style={scaleClientImages} src={client3} alt='client3' />
+          <img style={scaleClientImages} src={client4} alt='client4' />
+          <img style={scaleClientImages} src={client5} alt='client5' />
+          <img style={scaleClientImages} src={client6} alt='client6' />
+          <img style={scaleClientImages} src={client7} alt='client7' />
+          <img style={scaleClientImages} src={client8} alt='client8' />
+          <img style={scaleClientImages} src={client9} alt='client9' />
+          <img style={scaleClientImages} src={client10} alt='client10' />
+          <img style={scaleClientImages} src={client11} alt='client11' />
+          <img style={scaleClientImages} src={client12} alt='client12' />
+          <img style={scaleClientImages} src={client13} alt='client13' />
         </div>
 
         <div>
-          <h1>Weddings</h1>
+          <h1 style={headerFont}>Wedddings</h1>
         </div>
         <img style={scaleClientImages} src={wedding} alt='wedding' />
         <img style={scaleClientImages} src={wedding2} alt='wedding2' />
@@ -37,6 +53,11 @@ const Portfolio = () => {
       </animated.div>
     </div>
   );
+};
+
+const headerFont = {
+  fontFamily: 'Roboto, sans-serif',
+  textAlign: 'center'
 };
 
 const scaleClientImages = {
