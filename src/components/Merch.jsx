933c08999fcbf25b13cfc1e0c9f595cad785bdd6
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-03-13 16:05:08 -0700

    add singles merch pictures

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -4,6 +4,10 @@ import brushset2 from '../products/brushset2.jpg';
 import brushset3 from '../products/brushset3.jpg';
 import brushset4 from '../products/brushset4.jpg';
 import brush1 from '../products/brush1.jpg';
+import brush2 from '../products/brush2.jpg';
+import brush3 from '../products/brush3.jpg';
+import brush4 from '../products/brush4.jpg';
+
 const Merch = () => {
   return (
     <div>
@@ -17,6 +21,9 @@ const Merch = () => {
       <div style={alignImg}>
         <p style={divContentMargin}>Single</p>
         <img style={pictureSize} src={brush1} alt='brush' />
+        <img style={pictureSize} src={brush2} alt='brush' />
+        <img style={pictureSize} src={brush3} alt='brush' />
+        <img style={pictureSize} src={brush4} alt='brush' />
       </div>
     </div>
   );
